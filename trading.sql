921d7e1d7bc47bd04259f96d55ae814281f43fba
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-06-14 23:43:35 +0700

    update

--- trading.sql
+++ trading.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 23, 2024 at 08:26 AM
+-- Generation Time: Jun 14, 2024 at 06:42 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -20,6 +20,114 @@ SET time_zone = "+00:00";
 --
 -- Database: `trading`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `id` int(11) NOT NULL,
+  `kd_barang` varchar(100) NOT NULL,
+  `tracking` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `pn` varchar(100) NOT NULL,
+  `kd_sat` varchar(100) NOT NULL,
+  `beli` int(11) NOT NULL,
+  `jual` int(11) NOT NULL,
+  `min_beli` int(11) NOT NULL,
+  `min_stok` int(11) NOT NULL,
+  `max_stok` int(11) NOT NULL,
+  `stok` int(11) NOT NULL,
+  `hpp` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_gudang`
+--
+
+CREATE TABLE `tbl_gudang` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(50) NOT NULL,
+  `nama_gudang` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `telp` varchar(50) NOT NULL,
+  `user_add` int(11) NOT NULL,
+  `user_edit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kel_barang`
+--
+
+CREATE TABLE `tbl_kel_barang` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(100) NOT NULL,
+  `inisial` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL,
+  `user_add` int(11) NOT NULL,
+  `user_edit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_kode_trans`
+--
+
+CREATE TABLE `tbl_kode_trans` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `nama_form` varchar(100) NOT NULL,
+  `active` int(11) NOT NULL,
+  `user_add` int(11) NOT NULL,
+  `user_edit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `id` int(11) NOT NULL,
+  `kode` varchar(100) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `keterangan` text NOT NULL,
+  `active` int(11) NOT NULL,
+  `user_add` int(11) NOT NULL,
+  `user_edit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trans_header`
+--
+
+CREATE TABLE `trans_header` (
+  `id` int(11) NOT NULL,
+  `nodok` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL,
+  `ref` varchar(100) NOT NULL,
+  `kd_asal_gudang` varchar(100) NOT NULL,
+  `kd_tujuan_gudang` varchar(100) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` text NOT NULL,
+  `user_add` int(11) NOT NULL,
+  `user_edit` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -45,6 +153,42 @@ INSERT INTO `users` (`id`, `user_id`, `password`) VALUES
 --
 
 --
+-- Indexes for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_gudang`
+--
+ALTER TABLE `tbl_gudang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_kel_barang`
+--
+ALTER TABLE `tbl_kel_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_kode_trans`
+--
+ALTER TABLE `tbl_kode_trans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trans_header`
+--
+ALTER TABLE `trans_header`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -53,6 +197,42 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_barang`
+--
+ALTER TABLE `tbl_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_gudang`
+--
+ALTER TABLE `tbl_gudang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kel_barang`
+--
+ALTER TABLE `tbl_kel_barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_kode_trans`
+--
+ALTER TABLE `tbl_kode_trans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `trans_header`
+--
+ALTER TABLE `trans_header`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
